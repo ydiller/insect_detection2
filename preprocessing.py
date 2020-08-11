1b9a615f983ab6022241a89a3c94ef2e2a817c92
ydiller <59364326+ydiller@users.noreply.github.com>
2020-08-11 18:35:26 +0300

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,17 @@
 import os
 import shutil
 import numpy as np
+
+def train_test_split(dir_path):
+  cc_list=[]
+  bz_list=[]
+  for dir in os.walk(dir_path):
+    if "cc" in dir:
+      cc_list.append(dir)
+    if "bz" in dir:
+      bz_list.append(dir)
+  
+  print(cc_list)
+  
+def main():
+  train_test_split("images")
