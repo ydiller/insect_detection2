ef6db579e6d141e7263e2bff8f3fa688b4b82cd0
ydiller <59364326+ydiller@users.noreply.github.com>
2020-08-12 14:14:31 +0300

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -17,8 +17,8 @@ def train_test_split(dir_path):
   
         
   # create temporary fake labels     
-  cc_labels = np.ones(len(cc_list))
-  bz_labels = np.ones(len(bz_list))
+  #cc_labels = np.ones(len(cc_list))
+  #bz_labels = np.ones(len(bz_list))
   
   # create train/val/test subsets in [0.6:0.2:0.2] ratio using scikit
   #cc_train, cc_test, cc_train_labels, cc_test_labels = model_selection.train_test_split(cc_list, cc_labels, train_size=0.8,test_size=0.2, random_state=10)
@@ -26,13 +26,9 @@ def train_test_split(dir_path):
   cc_train, cc_test= model_selection.train_test_split(cc_list, train_size=0.8,test_size=0.2, random_state=10)
   cc_train, cc_val = model_selection.train_test_split(cc_train, train_size=0.75,test_size=0.25, random_state=10)
   
-  bz_train, bz_test, bz_train_labels, bz_test_labels = model_selection.train_test_split(bz_list, bz_labels, train_size=0.8,test_size=0.2, random_state=10)
-  bz_train, bz_val, bz_train_labels, bz_val_labels = model_selection.train_test_split(bz_train, bz_train_labels, train_size=0.75,test_size=0.25, random_state=10)
-  #random.shuffle(cc_list)
-  #random.shuffle(bz_list)
-  #cc_train = cc_list[:int(len(cc) * 0.6)]
-  #cc_val = cc_list[int(len(cc) * 0.6):int(len(cc) * 0.8)]
-  #cc_test = cc_list[int(len(cc) * 0.8):]
+  bz_train, bz_test = model_selection.train_test_split(bz_list, train_size=0.8,test_size=0.2, random_state=10)
+  bz_train, bz_val = model_selection.train_test_split(bz_train, train_size=0.75,test_size=0.25, random_state=10)
+
   print("bz train: ", bz_train)
   print("bz val: ", bz_val)
   print("bz test: ", bz_test)
