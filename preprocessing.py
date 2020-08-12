7cb504361916f0256c600658e4a205b23a2d7f7c
ydiller <59364326+ydiller@users.noreply.github.com>
2020-08-12 16:32:11 +0300

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -4,6 +4,7 @@ import numpy as np
 import sklearn.model_selection as model_selection
 import random
 
+# A method creates lists of the data splitted to train/val/test in [0.8:0.2:0.2] ratio
 def train_test_split(dir_path):
   cc_list=[]
   bz_list=[]
@@ -13,28 +14,16 @@ def train_test_split(dir_path):
         cc_list.append(dir)
       if "bz" in dir:
         bz_list.append(dir)
-   
-  
-        
-  # create temporary fake labels     
-  #cc_labels = np.ones(len(cc_list))
-  #bz_labels = np.ones(len(bz_list))
-  
-  # create train/val/test subsets in [0.6:0.2:0.2] ratio using scikit
-  #cc_train, cc_test, cc_train_labels, cc_test_labels = model_selection.train_test_split(cc_list, cc_labels, train_size=0.8,test_size=0.2, random_state=10)
-  #cc_train, cc_val, cc_train_labels, cc_val_labels = model_selection.train_test_split(cc_train, cc_train_labels, train_size=0.75,test_size=0.25, random_state=10)
+
   cc_train, cc_test= model_selection.train_test_split(cc_list, train_size=0.8,test_size=0.2, random_state=10)
   cc_train, cc_val = model_selection.train_test_split(cc_train, train_size=0.75,test_size=0.25, random_state=10)
   
   bz_train, bz_test = model_selection.train_test_split(bz_list, train_size=0.8,test_size=0.2, random_state=10)
   bz_train, bz_val = model_selection.train_test_split(bz_train, train_size=0.75,test_size=0.25, random_state=10)
-  
-  #print("bz train: ", bz_train)
-  #print("bz val: ", bz_val)
-  #print("bz test: ", bz_test)
-  
+ 
   return cc_train, cc_val , cc_test , bz_train, bz_val, bz_test
-  
+
+# An implementation of the shutil.copytree function that works with multiple copies to the same directory
 def copytree(src, dst, symlinks=False, ignore=None):
   for item in os.listdir(src):
       s = os.path.join(src, item)
@@ -43,13 +32,12 @@ def copytree(src, dst, symlinks=False, ignore=None):
           shutil.copytree(s, d, symlinks, ignore)
       else:
           shutil.copy2(s, d)
-          
+
 def main():
-  #train_test_split("/home/lab/dillery/images/")
   cc_train, cc_val , cc_test , bz_train, bz_val, bz_test = train_test_split("/images")
-  #os.makedirs("/images/cc/train")
-  #os.mkdir("/images/cc/test")
-  #os.mkdir("/images/cc/val")
+  os.makedirs("/images/cc/train")
+  os.mkdir("/images/cc/test")
+  os.mkdir("/images/cc/val")
   os.makedirs("/images/bz/train")
   os.mkdir("/images/bz/test")
   os.mkdir("/images/bz/val")
